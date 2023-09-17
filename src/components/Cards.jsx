d955bd4e82c5e47488cecd9eca98141e57cb21f8
IsmailDX <ismaildxx@gmail.com>
2023-09-17 20:46:31 +0400

    fixed some stuff

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = () => {
   return (
     <div className="w-full py-[10rem] px-4 bg-white">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+        <div className="w-full shadow-xl bg-gray-100 md:bg-white flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-20 mx-auto mt-[-3rem] bg-white"
             src={Single}
@@ -41,7 +41,7 @@ const Cards = () => {
             Start Trial
           </button>
         </div>
-        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+        <div className="w-full shadow-xl bg-gray-100 md:bg-white flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-20 mx-auto mt-[-3rem] bg-white"
             src={Triple}
